d9d149fa191917bf974cb731bb45d8e03ac46fda
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-02-07 07:37:41 -0500

    release version

--- README.md
+++ README.md
@@ -5,21 +5,27 @@ SignalStrengthDetect
 BoB 11th - Digital Forensics Track
 H4uN
 
+You must run with sudo privileges to obtain iwconfig privileges.
+
 Usage :
+`python3 SignalStrengthDetect.py <interface> <mac>`
 
 You need :
 Windows10  
 `pip install pyqtgraph`  
 `pip install pyqt5-tools`
+`pip install scapy`
 
 Ubuntu 22.04.1 LTS  
 `$ pip3 install pyqt5`  
 `$ sudo apt install python3-pyqt5`  
 `$ sudo apt install pyqt5-dev-tools`  
 `$ sudo apt install qttools5-dev-tools`
+`pip install scapy`
 
 If you are more than Python version 3.10 :
 Windows10  
 `pip install pyqtgraph`  
 `pip install pyqt5`  
 `pip install PyQt5Designer`
+`pip install scapy`
--- SignalStrengthDetect.py
+++ SignalStrengthDetect.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import *
+from scapy.all import *
 
 import pyqtgraph as pg
 import time
 import random
+import sys
 
-global realtime_check
+rssi = -100
+mac = ""
+interface =""
 realtime_check = True
 
 class TimeAxisItem(pg.AxisItem):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setLabel(text='Time(초)', units=None)
+        self.setLabel(text='Time(Seconds)', units=None)
         self.enableAutoSIPrefix(False)
 
     def tickStrings(self, values, scale, spacing):
-        """ override 하여, tick 옆에 써지는 문자를 원하는대로 수정함.
-            values --> x축 값들   ; 숫자로 이루어진 Itarable data --> ex) List[int]
-        """
-        # print("--tickStrings valuse ==>", values)
         return [time.strftime("%H:%M:%S", time.localtime(local_time)) for local_time in values]
 
 class ExampleWidget(QWidget):
@@ -27,8 +27,8 @@ class ExampleWidget(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.pw = pg.PlotWidget(
-            title="WiFi Signal Stregnth Detect",
-            labels={'left': 'WiFi Signal Strength'},
+            title="interface : "+str(interface) + "  |  WiFi mac : " + str(mac),
+            labels={'left': str(mac)+' Signal Strength'},
             axisItems={'bottom': TimeAxisItem(orientation='bottom')}
         )
 
@@ -46,23 +46,23 @@ class ExampleWidget(QWidget):
         self.pw.setYRange(-100, 0, padding=0)
 
         time_data = int(time.time())
-        self.pw.setXRange(time_data - 10, time_data + 1)  # 생략 가능.
+        self.pw.setXRange(time_data - 10, time_data + 1)
 
         self.pw.showGrid(x=True, y=True)
-        # self.pw.enableAutoRange()
-        self.pdi = self.pw.plot(pen='g')   # PlotDataItem obj 반환.
+        self.pdi = self.pw.plot(pen='g')   # PlotDataItem obj return
 
         self.plotData = {'x': [], 'y': []}
 
     def update_plot(self, new_time_data: int):
         data_sec = time.strftime("%S", time.localtime(new_time_data))
-        # self.plotData['y'].append(-int(data_sec))
-        Wifi_Signal_data = random.randint(-80, -20)
+        # Wifi_Signal_data = random.randint(-80, -20) # set data
+        sniff(iface=interface, prn = PacketHandler, count = 10)
+        Wifi_Signal_data = rssi
         self.plotData['y'].append(Wifi_Signal_data)
         self.plotData['x'].append(new_time_data)
 
         if (realtime_check):
-            self.pw.setXRange(new_time_data - 80, new_time_data + 20, padding=0)   # 항상 x축 시간을 최근 범위만 보여줌.
+            self.pw.setXRange(new_time_data - 80, new_time_data + 20, padding=0)   # always show X Line.
 
         self.pdi.setData(self.plotData['x'], self.plotData['y'])
 
@@ -71,20 +71,42 @@ class ExampleWidget(QWidget):
         QMessageBox.about(self, "message box", "Change Trace Mode!!!")
         realtime_check = not realtime_check
 
+def PacketHandler(pkt) :
+    global rssi
+    if pkt.haslayer(Dot11) :
+        if pkt.type == 0 and pkt.subtype == 8 :
+            if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
+                if pkt.addr2 == mac :
+                    try:
+                        rssi = -(256-pkt.notdecoded[0])
+                    except:
+                        rssi = -100
+                    # print ("WiFi signal strength:", rssi, "dBm of", pkt.addr2, pkt.info)
+
 if __name__ == "__main__":
-    import sys
+    import os
+
+    if len(sys.argv) != 3:
+        print("Wrong factor")
+        sys.exit()
+    
+    interface = sys.argv[1]
+    mac = sys.argv[2]
+    os.system("sudo ifconfig "+str(interface)+" down")
+    os.system("sudo iwconfig "+str(interface)+" mode monitor")
+    os.system("sudo ifconfig "+str(interface)+" up")
+    # sniff(iface=interface, prn = PacketHandler, count = 10)
 
     app = QApplication(sys.argv)
-
     ex = ExampleWidget()
-
     def get_data():
         new_time_data = int(time.time())
         ex.update_plot(new_time_data)
 
     mytimer = QTimer()
-    mytimer.start(500)  # 1초마다 갱신 위함...
+    mytimer.start(500)  # 0.5 seconds refresh
     mytimer.timeout.connect(get_data)
 
     ex.show()
     sys.exit(app.exec_())
+
